bd5284a9b0f5491ebffd0a5d2e5c18fbf69f5236
Lyrics_loop <jenishbhattarai2018@gmail.com>
2026-03-17 16:10:24 -0500

    balance.js

---
+++ main/commands/gambling/balance.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, 'data.json');
+
+function getData() {
+    if (!fs.existsSync(dataPath)) fs.writeFileSync(dataPath, '{}');
+    return JSON.parse(fs.readFileSync(dataPath));
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('balance')
+        .setDescription('Check your current balance'),
+
+    async execute(interaction) {
+        const data = getData();
+        const id = interaction.user.id;
+
+        if (!data[id]) data[id] = { money: 0 };
+
+        const embed = new EmbedBuilder()
+            .setTitle("💰 Your Balance")
+            .setDescription(`You currently have **$${data[id].money}**.`)
+            .setColor("Green")
+            .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+            .setFooter({ text: `Keep begging to earn more!` });
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
