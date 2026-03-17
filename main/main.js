6050dcd93f171a730994d37453676b9d84d0b306
Lyrics_loop <jenishbhattarai2018@gmail.com>
2026-03-17 00:38:12 -0500

    main.js

--- main/main.js
+++ main/main.js
@@ -1,1 +1,35 @@
+const {Client,  IntentsBitField} = require('discord.js');
+
+const cln = new Client({
+
+    intents: [
+        IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, 
+        ]
+})
+
+
+cln.on("clientReady", (c) => {
+    console.log(`Logged in as ${c.user.tag}`) });
+
+cln.on("messageCreate", (msg) => {
+
+    if (msg.author.bot) return;
+
+    console.log(`${msg.author.username}: ${msg.content}`);
+
+    if (msg.content.toLowerCase().includes('anyways')) {
+
+        if (msg.author.username !== "lyrics_loop") {
+
+            const emojis = ['❌', '©', '💥', '🔫'];
+
+            for (const emoji of emojis) {
+                msg.react(emoji).catch(console.error);
+            }
+        }
+    }
+});
+
+
+cln.login("BOT_TOKEN")
 
